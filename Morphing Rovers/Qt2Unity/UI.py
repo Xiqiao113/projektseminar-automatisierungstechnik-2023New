8820d3ef9e86ef60556eff57ead276cb6628a899
xiqiao113 <xiqiao.zhang@stud.tu-darmstadt.de>
2024-02-02 07:12:58 +0100

    text2  qiao

--- Morphing Rovers/Qt2Unity/UI.py
+++ Morphing Rovers/Qt2Unity/UI.py
@@ -4,45 +4,47 @@ from Aufgabe1Ui import *
 from Aufgabe2Ui import *
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
+import numpy as np
+
+global_x_value = None
+
 
 class LoginWindow(QMainWindow):
+    xValueUpdated = pyqtSignal(int)
 
-    # button_clicked = pyqtSignal(int)
     def __init__(self):
         super().__init__()
+        self.x = None
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.ui.Aufgabe1_btn.clicked.connect(self.getAufgabe1)
         self.ui.Aufgabe1_btn.clicked.connect(self.login_in1)
-        # self.ui.Aufgabe1_btn.clicked.connect(lambda: self.button_clicked.emit(1))
+        self.ui.Aufgabe2_btn.clicked.connect(self.getAufgabe2)
         self.ui.Aufgabe2_btn.clicked.connect(self.login_in2)
-        # self.ui.Aufgabe2_btn.clicked.connect(lambda: self.button_clicked.emit(2))
         self.show()
 
-
     def login_in1(self):
-        self.win = Aufgabe1Window()
-        self.close()
+        self.win = Aufgabe1Window(self)  # Pass self as parent
+        self.hide()
 
     def getAufgabe1(self):
-
         self.x = 1
-        return self.x
+        self.xValueUpdated.emit(self.x)
 
     def login_in2(self):
-        self.win = Aufgabe2Window()
-        self.close()
+        self.win = Aufgabe2Window(self)
+        self.hide()
 
     def getAufgabe2(self):
         self.x = 2
-        return self.x
-
+        self.xValueUpdated.emit(self.x)
 
 
 class Aufgabe1Window(QMainWindow):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
         self.ui = Ui_Aufgabe1_MainWindow()
         self.ui.setupUi(self)
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
@@ -51,20 +53,26 @@ class Aufgabe1Window(QMainWindow):
         self.ui.K_lineEdit.textChanged.connect(self.enable_submit_btn1)
         self.ui.S_lineEdit.textChanged.connect(self.enable_submit_btn1)
         self.ui.sub_1.clicked.connect(self.get_infos)
+        self.parent = parent
         self.show()
 
     def back(self):
-        self.win = LoginWindow()
+        if self.parent:
+            self.parent.show()  # Use the saved reference to redisplay the LoginWindow.
         self.close()
 
     def get_infos(self):
         Karte_Nr = self.ui.K_lineEdit.text()
         Szen_Nr = self.ui.S_lineEdit.text()
+        try:
+            Karte_Nr_int = int(Karte_Nr)
+            Szen_Nr_int = int(Szen_Nr)
 
-        # self.x, self.y = get_karteinfos(Karte_Nr, Szen_Nr)
-        return int(Karte_Nr),  int(Szen_Nr)
+            with open('infos.txt', 'a') as file:
+                file.write(f"\t{Karte_Nr_int}\t{Szen_Nr_int}\n")  # Formatting strings with newlines
+        except ValueError:
 
-
+            print("输入的值不是有效的整数")
 
 
     def enable_submit_btn1(self):
@@ -83,8 +91,8 @@ class Aufgabe1Window(QMainWindow):
 
 
 class Aufgabe2Window(QMainWindow):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
         self.ui = Ui_Aufgabe2_MainWindow()
         self.ui.setupUi(self)
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
@@ -95,10 +103,12 @@ class Aufgabe2Window(QMainWindow):
         self.ui.Probe_x.textChanged.connect(self.enable_submit_btn2)
         self.ui.Probe_y.textChanged.connect(self.enable_submit_btn2)
         self.ui.sub_2.clicked.connect(self.submit)
+        self.parent = parent
         self.show()
 
     def back(self):
-        self.win = LoginWindow()
+        if self.parent:
+            self.parent.show()  # Use the saved reference to redisplay the LoginWindow.
         self.close()
 
     def submit(self):
@@ -108,7 +118,16 @@ class Aufgabe2Window(QMainWindow):
         P_x = self.ui.Probe_x.text()
         P_y = self.ui.Probe_y.text()
 
-        self.x, self.y = get_coordinates(R_x, R_y, P_x, P_y)
+        try:
+            R_x_float = float(R_x)
+            R_y_float = float(R_y)
+            P_x_float = float(P_x)
+            P_y_float = float(P_y)
+            # write to a file
+            with open('infos.txt', 'a') as file:  # Using the append mode
+                file.write(f"\t{R_x_float}\t{R_y_float}\t{P_x_float}\t{P_y_float}\n")  # Use \t for Tab Separation
+        except ValueError:
+            print("not valuable float")
 
     def enable_submit_btn2(self):
         R_x = self.ui.Rover_x.text()
@@ -122,45 +141,28 @@ class Aufgabe2Window(QMainWindow):
             R_y = float(R_y)
             P_x = float(P_x)
             P_y = float(P_y)
-            if  0 <= R_x <= 2800 and 0 <= R_y <= 2800 and 0 <= P_x <= 2800 and 0 <= P_y <= 2800:
+            if 0 <= R_x <= 2800 and 0 <= R_y <= 2800 and 0 <= P_x <= 2800 and 0 <= P_y <= 2800:
                 self.ui.sub_2.setEnabled(True)
             else:
                 self.ui.sub_2.setEnabled(False)
         except ValueError:
             self.ui.sub_2.setEnabled(False)
 
+# from texttext import collect_data
+def handleXValueUpdated(value):
+    # print("new x:", value)
+    with open('infos.txt', 'w') as file:
+        file.write(str(value))
+
 
 if __name__ == '__main__':
-    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling) #
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)  #
     app = QApplication(sys.argv)
-
-    def get_coordinates(x1_str, y1_str, x2_str, y2_str):
-        x1 = float(x1_str)
-        y1 = float(y1_str)
-        x2 = float(x2_str)
-        y2 = float(y2_str)
-
-        Rover_coord = [x1, y1]
-        Probe_coord = [x2, y2]
-
-        print("Rover:  =", Rover_coord)
-        print("Probe:  =", Probe_coord)
-
-        return  Rover_coord, Probe_coord
-
-    def get_karteinfos(K_str, S_str):
-
-        K_Nr = int(K_str)
-        S_Nr = int(S_str)
-
-        print("the chosen Szenario is map:", K_Nr, 'Senario:', S_Nr)
-
-        return K_Nr, S_Nr
-
-
-
     win = LoginWindow()
-    sys.exit(app.exec_())
+    win.xValueUpdated.connect(handleXValueUpdated)  # Connecting Signals to Slot Functions
+    win.show()
+    result = app.exec_()
 
+    sys.exit(result)
 
 
